9abdde9518b92679bba0df22af3d85fd94478107
diversemix <diversemix@gmail.com>
2015-12-31 23:47:48 +0000

    Sort out the install script and add tmux config

--- install.sh
+++ install.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+DOT_DIR=~/dotfiles
+BACKUP_DIR=~/dotfiles/old
+FILE_LIST="bashrc vimrc vim"
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
+echo -n "  Creating ${BACKUP_DIR} ..."
+mkdir -p ${BACKUP_DIR}
+echo done.
 
-##########
+cd ${DOT_DIR}
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+echo "  Moving existing dotfiles in ${HOME} to ${BACKUP_DIR} "
+for file in ${FILE_LIST}; do
+    mv ~/.$file ${BACKUP_DIR}
+    echo "    Creating symlink ${DOT_DIR}/${file} ~/.${file}"
+    ln -s $DOT_DIR/${file} ~/.${file}
 done
 
-# finally install the scripts folder
-ln -s $dir/scripts ~/scripts
