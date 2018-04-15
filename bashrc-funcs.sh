de4dcfbdeaee6a6dd5d38528f104f23ec30295b0
diversemix <diversemix@gmail.com>
2018-04-15 17:13:31 +0100

    refactor functinos

--- bashrc-funcs.sh
+++ bashrc-funcs.sh
@@ -4,19 +4,60 @@ function topmem()
     ps -eo size,pid,user,command --sort -size | \
         awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s" , $x) } print "" }' | \
         head
-
 }
-
 
 function set-title() {
     TITLE=$1
     PROMPT_COMMAND='echo -ne "\033]0;${TITLE}\007"'
 }
 
-
 function swap()
 {
     awk '{ t = $1; $1 = $2; $2 = t; print; }'
+}
+short_pwd() {
+  local pwd=$(pwd)
+  pwd=${pwd/#$HOME/\~}
+  sed 's:\([^/]\)[^/]*/:\1/:g' <<<"$pwd"
+}
+
+git_info() {
+  if [ -d .git ]; then
+    #local pwd=$(pwd)
+    #echo ${pwd} $(pwd) $PWD
+    local branch=$(git status -s -b | head -n 1 | sed -e 's/## //g' | sed -e 's/\.\.\..*//g')
+    local remote=$(git remote -v | grep fetch | cut -f2 | cut -d ' ' -f1)
+    local remote_short=$(echo $remote | \
+      sed -e 's/https:\/\/github.com\///g' | \
+      sed -e 's/.git$//g' | \
+      sed -e 's/git@github.com://g')
+    echo "$remote_short${TEAL}:$branch"
+
+    staged=$(git status -s | cut -c 1| uniq -c| sed -e 's/ //g' | grep -e '..' |tr -s ' ' | xargs)
+    locally=$(git status -s | cut -c 2| uniq -c| sed -e 's/ //g' | grep -e '..' |tr -s ' ' | xargs)
+
+    printf "${RED}[$locally]${TEAL}($staged)"
+  fi
+}
+
+prompt_prefix() {
+  if [ -d .git ]; then
+    git_info
+  else
+    DISK=$(df -h / | tr -s ' ' | cut -d ' ' -f5 | tail -n 1 | cut -d '%' -f1)
+    print_value_with_color "⛃ " ${DISK}
+
+    MEM=$(free | grep Mem | awk  '{printf ("%2.0f", $3/$2 * 100.0) }')
+    print_value_with_color "🗇 " ${MEM}
+    #print_command_with_color "🖧 " "test_network"
+
+    # disk, memory, network, dropbox
+    echo "| ${YELLOW}$(hostname)${RESET}"
+  fi
+}
+
+last_result() {
+  if [ $? = 0 ]; then echo "${GOOD}"; else echo "${BAD}"; fi
 }
 
 print_value_with_color() {
--- bashrc-prompt.sh
+++ bashrc-prompt.sh
@@ -1,51 +1,6 @@
 source ~/dotfiles/bashrc-colors.sh
 
 # Prompt Section
-# export PS1="\n\[\e[1;32m\]\w\[\e[1;31m\] [\!] \[\e[1;33m\]\$\[\e[m\] "
-short_pwd() {
-  local pwd=$(pwd)
-  pwd=${pwd/#$HOME/\~}
-  sed 's:\([^/]\)[^/]*/:\1/:g' <<<"$pwd"
-}
-
-git_info() {
-  if [ -d .git ]; then
-    #local pwd=$(pwd)
-    #echo ${pwd} $(pwd) $PWD
-    local branch=$(git status -s -b | head -n 1 | sed -e 's/## //g' | sed -e 's/\.\.\..*//g')
-    local remote=$(git remote -v | grep fetch | cut -f2 | cut -d ' ' -f1)
-    local remote_short=$(echo $remote | \
-      sed -e 's/https:\/\/github.com\///g' | \
-      sed -e 's/.git$//g' | \
-      sed -e 's/git@github.com://g')
-    echo "$remote_short${TEAL}:$branch"
-
-    staged=$(git status -s | cut -c 1| uniq -c| sed -e 's/ //g' | grep -e '..' |tr -s ' ' | xargs)
-    locally=$(git status -s | cut -c 2| uniq -c| sed -e 's/ //g' | grep -e '..' |tr -s ' ' | xargs)
-
-    printf "${RED}[$locally]${TEAL}($staged)"
-  fi
-}
-
-prompt_prefix() {
-  if [ -d .git ]; then
-    git_info
-  else
-    DISK=$(df -h / | tr -s ' ' | cut -d ' ' -f5 | tail -n 1 | cut -d '%' -f1)
-    print_value_with_color "⛃ " ${DISK}
-
-    MEM=$(free | grep Mem | awk  '{printf ("%2.0f", $3/$2 * 100.0) }')
-    print_value_with_color "🗇 " ${MEM}
-    #print_command_with_color "🖧 " "test_network"
-
-    # disk, memory, network, dropbox
-    echo "| ${YELLOW}$(hostname)${RESET}"
-  fi
-}
-
-last_result() {
-  if [ $? = 0 ]; then echo "${GOOD}"; else echo "${BAD}"; fi
-}
 
 LAST_RESULT="last_result"
 THE_PWD="short_pwd"
