93309b668cde2a13b1786c182546ffbd2b927f80
diversemix <diversemix@gmail.com>
2020-03-13 11:43:55 +0000

    updates

--- apps/install-python.sh
+++ apps/install-python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing python ..."
-sudo apt-get install libssl-dev openssl python-pip python-venv virtualenvwrapper
+sudo apt-get install libssl-dev openssl python-pip python-venv virtualenvwrapper virtualenv
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
@@ -9,4 +9,11 @@ for ver in 5 6 7 8
 do
   sudo apt install python3.$ver python3.$ver-dev
 done
+
+sudo pip2 install virtualenvwrapper
+sudo pip3 install virtualenvwrapper
+
+echo "Setting up virual envs..."
+export VE=${HOME}/.virtualenvs
+mkdir $VE
 
--- bashrc/vars.sh
+++ bashrc/vars.sh
@@ -6,6 +6,9 @@ export PATH=$PATH:$GOPATH/bin
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
+export WORKON_HOME=${HOME}/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
+
 # Uncomment the following line if you don't like systemctl's auto-paging feature:
 # export SYSTEMD_PAGER=
 
