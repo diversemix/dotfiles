e2393685d730a3aafa0b5656bc60fb94c4d884ac
diversemix <diversemix@gmail.com>
2020-06-16 14:35:44 +0100

    update i3 comfig and git editor

--- bashrc/vars.sh
+++ bashrc/vars.sh
@@ -1,11 +1,11 @@
-export SVN_EDITOR=vim
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export GOPATH=$HOME/go
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
-export VISUAL=vim
-export EDITOR="$VISUAL"
+export EDITOR=nvim
+export VISUAL=nvim
+export SVN_EDITOR=nvim
 
 export WORKON_HOME=${HOME}/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
