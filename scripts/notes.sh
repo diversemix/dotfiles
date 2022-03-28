db9595cee0389d6ebb25fe7aa7a8f77f87e57523
Peter Hooper <diversemix@gmail.com>
2022-03-28 13:21:29 +0100

    one file per month

--- scripts/notes.sh
+++ scripts/notes.sh
@@ -2,14 +2,14 @@
 #
 # Inspired by https://github.com/CalinLeafshade/dots/blob/master/bin/bin/notetaker
 #
-noteFilename="$HOME/Dropbox/Documents/Notes/md/Note-$(date +%Y-%m-%d).md"
+noteFilename="$HOME/Dropbox/Documents/Notes/md/Notes-$(date +%Y-%m).md"
 
 if [ ! -f $noteFilename ]; then
-  echo "# Notes for $(date +%Y-%m-%d)" > $noteFilename
+  echo "# Notes for $(date +%Y-%m)" > $noteFilename
 fi
 
 vim -c "norm Go" \
-  -c "norm Go## $(date +%H:%M)" \
+  -c "norm Go## $(date '+%A %d-%B, %Y %H:%M')" \
   -c "norm G2o" \
   -c "norm zz" \
   -c "startinsert" $noteFilename
