f94cce47b36d1ed603e110f630a96014257c4227
diversemix <diversemix@gmail.com>
2022-04-25 21:54:45 +0100

    hack for search notes

--- scripts/searchNotes.sh
+++ scripts/searchNotes.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-j
-vim "+cd $HOME/Dropbox/Documents/Notes/md/" "+Ag" 
+
+# Hack - see https://github.com/junegunn/fzf.vim/issues/1231#issuecomment-774782641
+vim -c "cd $HOME/Dropbox/Documents/Notes/md/" \
+  -c "autocmd VimEnter * call timer_start(0, {->execute('Ag $1')})"
