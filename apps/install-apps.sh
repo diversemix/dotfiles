bdfb132fcfaee50c5686e922f590def3c1978896
diversemix <diversemix@gmail.com>
2022-05-07 18:24:16 +0100

    add how-2 and emoj to app install

--- apps/install-apps.sh
+++ apps/install-apps.sh
@@ -3,14 +3,11 @@
 echo "  Ensuring the app-list is installed...."
 cat app-list | cut -d '#' -f1 | xargs sudo apt install -y
 
+npm i -g how-2 emoj
+
 echo Setting up git...
 git config --global user.email "diversemix@gmail.com"
 git config --global user.name "diversemix"
-
-echo Installing bat...
-export VER="0.12.1"
-wget https://github.com/sharkdp/bat/releases/download/v${VER}/bat_${VER}_amd64.deb
-sudo dpkg -i bat_${VER}_amd64.deb
 
 echo "Install tab completion for docker..."
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.3/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
@@ -18,7 +15,6 @@ sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.3/contrib/com
 echo Ensuring you can run docker without root access...
 sudo usermod -aG docker ${USER}
 echo *** YOU NEED TO LOGIN AGAIN ***
-
 
 
 if [ ! -d ~/Dropbox ] ; then
