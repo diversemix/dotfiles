011898676fe0c2b8bfb31d6f7a2161ff4aa824e7
diversemix <diversemix@gmail.com>
2022-09-23 16:38:26 +0100

    useful updates

--- scripts/notes.sh
+++ scripts/notes.sh
@@ -5,6 +5,10 @@
 cd "$HOME/Dropbox/Documents/Notes/md/"
 noteFilename="Notes-$(date +%Y-%m).md"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 if [ ! -f $noteFilename ]; then
   echo "# Notes for $(date +%Y-%m)" > $noteFilename
 fi
