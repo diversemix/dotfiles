61a04a9b4a7a01dc3279fa223582b996a15280b1
diversemix <diversemix@gmail.com>
2022-11-08 14:41:35 +0000

    improve search scripts

--- scripts/searchNotes.sh
+++ scripts/searchNotes.sh
@@ -6,4 +6,5 @@ export NVM_DIR="$HOME/.nvm"
 
 # Hack - see https://github.com/junegunn/fzf.vim/issues/1231#issuecomment-774782641
 vim -c "cd $HOME/Dropbox/Documents/Notes/md/" \
-  -c "autocmd VimEnter * call timer_start(0, {->execute('Ag $1')})"
+  -c "Ag"
+#-c "autocmd VimEnter * call timer_start(0, {->execute('Ag $1')})"
